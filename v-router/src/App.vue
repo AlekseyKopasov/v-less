3d4be1b9edbf390413d039d2ba3e134cfef7419a
Aleksey Kopasov <alesey.k1@gmail.com>
2020-12-16 23:10:38 +0300

    Добавляет Ðnavbar

--- v-router/src/App.vue
+++ v-router/src/App.vue
@@ -7,10 +7,14 @@
           <div class="navbar-content">
             <div class="logo">VUE-CLI</div>
             <ul class="navbar-list">
-              <li class="navbar-item">
-                <a href="#" class="navbar-link">Home</a>
-              </li><li class="navbar-item">
-                <a href="#" class="navbar-link">Example</a>
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link
+                    :to="link.url"
+                    class="navbar-link"
+                    exact
+                >
+                  {{ link.title }}
+                </router-link>
               </li>
             </ul>
           </div>
@@ -25,5 +29,18 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    links: [
+      {
+        title: 'Home',
+        url: '/',
+      },
+      {
+        title: 'Example',
+        url: '/example',
+      },
+    ],
+  }),
+};
 </script>
--- v-router/src/pages/Example.vue
+++ v-router/src/pages/Example.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="wrapper-content">
+
+  <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
         <h1>Example Page</h1>
       </div>
     </section>
   </div>
+
 </template>
 
 <script>
