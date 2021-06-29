f3af42c18e7743dad9e4fdb5e35308959ed6c192
Aleksey Kopasov <alesey.k1@gmail.com>
2021-06-30 00:16:01 +0300

    добавляет стили

---
+++ v-blog/components/system/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <div class="logo">Blog</div>
+          <ul class="navbar-list">
+            <li class="navbar-item" v-for="link in links" :key="link.title">
+              <nuxt-link :to="link.url" class="navbar-link" exact>
+                {{ link.title }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [{ title: "Home", url: "/" }],
+  }),
+};
+</script>
