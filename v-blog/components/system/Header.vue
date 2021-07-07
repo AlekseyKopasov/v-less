3c1047ffd371d7333676b286a41b1ad6ceb4d1e3
Aleksey Kopasov <alesey.k1@gmail.com>
2021-07-08 00:28:08 +0300

    добавляет error layout

--- v-blog/components/system/Header.vue
+++ v-blog/components/system/Header.vue
@@ -3,7 +3,7 @@
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
-          <div class="logo">Blog</div>
+          <div class="logo"><nuxt-link to="/">Blog</nuxt-link></div>
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link in links" :key="link.title">
               <nuxt-link :to="link.url" class="navbar-link" exact>
@@ -21,7 +21,6 @@
 export default {
   data: () => ({
     links: [
-      { title: "Home", url: "/" },
       { title: "About", url: "/about" },
       { title: "Blog", url: "/blog" },
     ],
@@ -30,6 +29,10 @@ export default {
 </script>
 
 <style lang="scss">
+.logo a {
+  color: #333333;
+}
+
 .navbar-link {
   &.nuxt-link-exact-active {
     color: #4156de;
