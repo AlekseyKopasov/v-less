653b84d8897aa4be379dc3cb22af5e9f79c6e9ed
Aleksey Kopasov <alesey.k1@gmail.com>
2021-07-10 00:18:46 +0300

    WIP: добавляет админку

---
+++ v-blog/layouts/admin.vue
@@ -0,0 +1,19 @@
+ <template>
+  <div class="wrapper">
+    <Header />
+    <div class="wrapper-content wrapper-content--fixed">
+      <Intro title="Admin page">
+        <nuxt-link to="/admin" class="link linkWhite"> Admin </nuxt-link>
+      </Intro>
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/system/Header";
+
+export default {
+  components: { Header },
+};
+</script>
